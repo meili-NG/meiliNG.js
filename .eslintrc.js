a82916bc8e16a41020b15810b7c64c540a9ce0b4
Joosung Park <iam@hyperlapse.dev>
2021-05-15 18:02:16 +0900

    chore: add prisma

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,36 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+  'env': {
+    'es2021': true,
+    'node': true,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
+  'extends': [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
   },
 };

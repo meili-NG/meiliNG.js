d55991f43ebe64a9151aa5ddd4839e21c40c7135
Joosung Park <iam@hyperlapse.dev>
2021-05-15 17:45:08 +0900

    chore: use fastify instead express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().then(() => {
+  console.log('Meiling is listening...');
+});

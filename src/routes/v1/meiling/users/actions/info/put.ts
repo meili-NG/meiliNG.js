c80f775fde59b98d54bedc80cedc5eb9cfe72a86
alex4386 <psh010209@gmail.com>
2021-06-02 02:01:16 +0900

    chore: implement update on user profile

--- src/routes/v1/meiling/users/actions/info/put.ts
+++ src/routes/v1/meiling/users/actions/info/put.ts
@@ -1,13 +1,24 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { FastifyRequestWithSession } from '../../..';
+import { Utils } from '../../../../../../common';
 import { getSanitizedUser } from '../../../../../../common/sanitize';
+import { getPrismaClient } from '../../../../../../resources/prisma';
 import { sendMeilingError } from '../../../error';
 import { MeilingV1ErrorType } from '../../../interfaces';
+
+interface UserUpdateRequestBody {
+  birthday?: string | number;
+  familyName?: string;
+  givenName?: string;
+  middleName?: string | null;
+  name?: string;
+}
 
 export async function userUpdateInfo(req: FastifyRequest, rep: FastifyReply) {
   const session = (req as FastifyRequestWithSession).session;
   const userRawSession = session.user;
 
+  const body = req.body as UserUpdateRequestBody;
   const userId = (req.params as any)?.userId;
 
   if (userRawSession && userRawSession.length > 0) {
@@ -15,10 +26,20 @@ export async function userUpdateInfo(req: FastifyRequest, rep: FastifyReply) {
       const users = userRawSession.filter((n) => n.id === userId);
 
       if (users.length === 1) {
+        await getPrismaClient().user.update({
+          where: {
+            id: users[0].id,
+          },
+          data: {
+            birthday: body.birthday ? new Date(body.birthday) : undefined,
+            familyName: Utils.isNotBlank(body.familyName) ? body.familyName : undefined,
+            givenName: Utils.isNotBlank(body.givenName) ? body.givenName : undefined,
+            middleName: Utils.isNotBlank(body.middleName) ? body.middleName?.normalize('NFC') : undefined,
+            name: Utils.isNotBlank(body.name) ? body.name : undefined,
+          },
+        });
+
         const user = await getSanitizedUser(users[0].id);
-
-        // do some magic.
-
         rep.send(user);
         return;
       } else {

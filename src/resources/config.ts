1296534b5e1ebb2834048bdcd0c5ec0bb9dd0679
alex4386 <psh010209@gmail.com>
2021-06-22 22:54:01 +0900

    chore: add auto-fallback to env

--- src/resources/config.ts
+++ src/resources/config.ts
@@ -1,6 +1,10 @@
+import fs from 'fs';
+import path from 'path';
 import { ConfigInterface } from '../interface';
 
+const configFileExists = fs.existsSync(path.join(__dirname, '../../config.js'));
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const config: ConfigInterface = require('../../config');
+const config: ConfigInterface = configFileExists ? require('../../config') : require('../../config.env');
 
 export default config;

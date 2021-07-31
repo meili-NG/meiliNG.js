f79e7063adfc9cb638d377a790ca4a1a16435c6d
alex4386 <psh010209@gmail.com>
2021-08-01 00:26:07 +0900

    chore: add 2fa

--- src/routes/v1/meiling/users/actions/security/index.ts
+++ src/routes/v1/meiling/users/actions/security/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import user2FAPlugin from './2fa';
 import userPasswordsPlugin from './passwords';
 
 function userSecurityPlugin(app: FastifyInstance, opts: FastifyPluginOptions, done: () => void): void {
   app.register(userPasswordsPlugin, { prefix: '/passwords' });
+  app.register(user2FAPlugin, { prefix: '/2fa' });
 
   done();
 }

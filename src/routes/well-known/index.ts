886626d6ab75bb6f475f9e900ebda9307f6619d7
Alex4386 <psh010209@gmail.com>
2021-11-19 23:49:19 +0900

    feat: add `/oauth-authorization-server` either

--- src/routes/well-known/index.ts
+++ src/routes/well-known/index.ts
@@ -4,6 +4,9 @@ import openIdConfigurationHandler from './openid-configuration';
 function wellKnownPlugin(app: FastifyInstance, opts: FastifyPluginOptions, done: () => void): void {
   app.get('/openid-configuration', openIdConfigurationHandler);
 
+  // .well-known/oauth-authorization-server
+  app.get('/oauth-authorization-server', openIdConfigurationHandler);
+
   done();
 }
 

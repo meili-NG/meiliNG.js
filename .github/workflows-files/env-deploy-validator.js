6aecb841738fa35aad229d8d8d11624a528b4d48
Alex4386 <psh010209@gmail.com>
2021-11-20 11:42:19 +0900

    fix: deploy key setup

--- .github/workflows-files/env-deploy-validator.js
+++ .github/workflows-files/env-deploy-validator.js
@@ -9,9 +9,30 @@ const projectRoot = path.join(__dirname, "..", "..");
 const envFilePath = path.join(projectRoot, ".env");
 
 const envFileCheck = fs.existsSync(envFilePath);
-if (!envFileCheck) process.exit(1);
+if (!envFileCheck) {
+  console.error(`${
+    chalk.redBright(
+      chalk.bold`Error: .env configuration missing!`
+  )}`);
+  process.exit(1);
+} 
 
 const values = dotenv.parse(fs.readFileSync(envFilePath, {encoding: 'utf-8'}));
+if (Object.keys(values).length === 0) {
+  console.error(`${
+    chalk.redBright(
+      chalk.bold`Error: invalid .env configuration!`
+  )}`);
+  process.exit(1);
+} 
 
-console.log("Debug:");
-console.log(Object.keys(values));
+const key = process.env.DEPLOY_KEY
+if (!key) {
+  console.error(`${
+    chalk.redBright(
+      chalk.bold`Error: invalid deploy key!`
+  )}`);
+  process.exit(1);
+}
+
+

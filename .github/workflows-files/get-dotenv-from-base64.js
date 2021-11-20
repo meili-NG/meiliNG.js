a7c165f117570916b8095ee9dd5e3cf074c9c066
Alex4386 <psh010209@gmail.com>
2021-11-20 10:34:52 +0900

    ci: making error message more straight forward

--- .github/workflows-files/get-dotenv-from-base64.js
+++ .github/workflows-files/get-dotenv-from-base64.js
@@ -11,7 +11,13 @@ const projectRoot = path.join(__dirname, "..", "..");
 const envFilePath = path.join(projectRoot, ".env");
 
 const base64 = process.env.DEPLOY_ENV_BASE64;
-if (!base64) process.exit(1);
+if (!base64) {
+  console.error(`${
+    chalk.redBright(
+      chalk.bold`Error: base64 value missing!`
+  )}`);
+  process.exit(1);
+}
 
 const result = Buffer.from(base64, 'base64');
 fs.writeFileSync(envFilePath, result);

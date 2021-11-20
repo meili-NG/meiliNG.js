9d8fc8b1ca705f4480e7eb4e9517fcc773a7382b
Alex4386 <psh010209@gmail.com>
2021-11-20 12:08:38 +0900

    chore: no strict host checking I know this is a bad practice but.... I don't know how to resolve this at right now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,7 +28,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'https://github.com/meiling-gatekeeper/meiling',
       path: process.env.DEPLOY_PRODUCTION_PATH,
-      'pre-deploy-local': `scp -Cr ./.env ${process.env.DEPLOY_PRODUCTION_KEY_PATH ? "-i "+process.env.DEPLOY_PRODUCTION_KEY_PATH : ""} ${process.env.DEPLOY_PRODUCTION_USER}@${process.env.DEPLOY_PRODUCTION_HOST}:${process.env.DEPLOY_PRODUCTION_PATH}/current`,
+      'pre-deploy-local': `scp -o StrictHostKeyChecking=no -Cr ./.env ${process.env.DEPLOY_PRODUCTION_KEY_PATH ? "-i "+process.env.DEPLOY_PRODUCTION_KEY_PATH : ""} ${process.env.DEPLOY_PRODUCTION_USER}@${process.env.DEPLOY_PRODUCTION_HOST}:${process.env.DEPLOY_PRODUCTION_PATH}/current`,
       'post-deploy': `yarn && yarn build && yarn generate && yarn prisma migrate deploy && pm2 startOrRestart ecosystem.config.js`,
     },
   },

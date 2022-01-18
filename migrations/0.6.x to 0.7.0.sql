944d55a14c78d16aa6592624479448502b699bc3
Alex4386 <psh010209@gmail.com>
2022-01-18 10:53:54 +0000

    fix: silly mistake on table name

--- migrations/0.6.x to 0.7.0.sql
+++ migrations/0.6.x to 0.7.0.sql
@@ -4,4 +4,4 @@
  * See: https://github.com/meiling-gatekeeper/meiling/commit/cbe0e77b86c774d5e80835ed40262ef950b48f81#commitcomment-64032495
  */
 
-ALTER TABLE `Authorization` TO `Authentication`;
+ALTER TABLE `Authorization` RENAME TO `Authentication`;
